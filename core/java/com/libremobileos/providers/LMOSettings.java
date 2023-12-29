5b91e6de9dc973ed3b919f20806fa723852853d8
Erfan Abdi <erfangplus@gmail.com>
2023-12-29 12:00:21 +0330

    core: Add double tap to sleep gesture
    Change-Id: Ib15d90a033a73f7a472f5f6dbabd42a96a7c330a

--- core/java/com/libremobileos/providers/LMOSettings.java
+++ core/java/com/libremobileos/providers/LMOSettings.java
@@ -676,6 +676,13 @@ public final class LMOSettings {
          */
         public static final String DISPLAY_PICTURE_ADJUSTMENT = "display_picture_adjustment";
 
+        /**
+         * Enable statusbar double tap gesture on to put device to sleep
+         * 
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
     }
 
     /**
