5b3e8df000983b1d84675573a07cce0dda8062ff
Alexander Martinz <amartinz@shiftphones.com>
2025-04-03 21:07:19 +0200

    frameworks: Intent: unhide actions
    These may be interesting to other apps.
    
    Change-Id: If662e4edc71d21026af2c143ab14755e115870a2
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- core/java/com/libremobileos/content/Intent.java
+++ core/java/com/libremobileos/content/Intent.java
@@ -29,7 +29,6 @@ public class Intent {
      * <p>Input: {@link #EXTRA_LONG_PRESS_RELEASE} is set to true if the long press
      * is released
      * <p>Output: Nothing
-     * @hide
      */
     public static final String ACTION_RECENTS_LONG_PRESS =
             "lineageos.intent.action.RECENTS_LONG_PRESS";
@@ -38,7 +37,6 @@ public class Intent {
      * This field is part of the intent {@link #ACTION_RECENTS_LONG_PRESS}.
      * The type of the extra is a boolean that indicates if the long press
      * is released.
-     * @hide
      */
     public static final String EXTRA_RECENTS_LONG_PRESS_RELEASE =
             "lineageos.intent.extra.RECENTS_LONG_PRESS_RELEASE";
@@ -47,7 +45,6 @@ public class Intent {
      * Broadcast action: notify the system that the user has performed a gesture on the screen
      * to launch the camera. Broadcast should be protected to receivers holding the
      * {@link Manifest.permission#STATUS_BAR_SERVICE} permission.
-     * @hide
      */
     public static final String ACTION_SCREEN_CAMERA_GESTURE =
             "lineageos.intent.action.SCREEN_CAMERA_GESTURE";
@@ -57,14 +54,12 @@ public class Intent {
      * Runs when the service receives the signal the device has booted, but
      * should happen before {@link android.content.Intent#ACTION_BOOT_COMPLETED}.
      *
-     * @hide
      */
     public static final String ACTION_INITIALIZE_LINEAGE_HARDWARE =
             "lineageos.intent.action.INITIALIZE_LINEAGE_HARDWARE";
 
     /**
      * Broadcast action: lid state changed
-     * @hide
      */
     public static final String ACTION_LID_STATE_CHANGED =
             "lineageos.intent.action.LID_STATE_CHANGED";
@@ -72,14 +67,12 @@ public class Intent {
     /**
      * This field is part of the intent {@link #ACTION_LID_STATE_CHANGED}.
      * Intent extra field for the state of lid/cover
-     * @hide
      */
     public static final String EXTRA_LID_STATE =
             "lineageos.intent.extra.LID_STATE";
 
     /**
      * Broadcast action: notify SystemUI that LiveDisplay service has finished initialization.
-     * @hide
      */
     public static final String ACTION_INITIALIZE_LIVEDISPLAY =
             "lineageos.intent.action.INITIALIZE_LIVEDISPLAY";
