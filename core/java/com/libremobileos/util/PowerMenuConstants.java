87aa6e8a25a1848be3dd611dae8f19af1c595009
LuK1337 <priv.luk@gmail.com>
2025-06-22 17:18:52 +0200

    frameworks: Add GLOBAL_ACTION_KEY_STANDBY
    Change-Id: I507eb1ce6534286dd4745f85a5d86c0598757b17

--- core/java/com/libremobileos/util/PowerMenuConstants.java
+++ core/java/com/libremobileos/util/PowerMenuConstants.java
@@ -35,6 +35,7 @@ public class PowerMenuConstants {
     public static final String GLOBAL_ACTION_KEY_DEVICECONTROLS = "devicecontrols";
     public static final String GLOBAL_ACTION_KEY_PANIC = "panic";
     public static final String GLOBAL_ACTION_KEY_SYSTEM_UPDATE = "system_update";
+    public static final String GLOBAL_ACTION_KEY_STANDBY = "standby";
 
     /**
      * Advanced restart menu actions
@@ -68,6 +69,7 @@ public class PowerMenuConstants {
         GLOBAL_ACTION_KEY_PANIC,
         GLOBAL_ACTION_KEY_LOGOUT,
         GLOBAL_ACTION_KEY_SYSTEM_UPDATE,
+        GLOBAL_ACTION_KEY_STANDBY,
     };
 
     public static String[] getAllActions() {
